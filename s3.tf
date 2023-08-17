bdcacee1d3b89ffdbf8ddde770142543a06caaee
srikanthv999 <107660577+srikanthv999@users.noreply.github.com>
2023-08-17 20:49:10 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "s3bkt-dev" {
   bucket = var.s3bktName
 
   tags = {
-    Name        = "Tf-S3-Bucket"
+    Name        = "Tf-S3-Bucket-dev"
     Environment = "Dev"
     ProjectCode = "Mn10091"
   }
